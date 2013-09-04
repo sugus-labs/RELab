fb365af1688575c558529b2d0c92c1f71bca6491
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-04 10:37:07 +0200

    Added functions to make post and change the appearance

--- RELab/views.py
+++ RELab/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render, render_to_response, redirect
 import json
 
 def home(request):
-    return render(request, 'home.html')
-
+	if request.method == 'GET':
+		return render(request, 'home.html')
+	elif request.method == 'POST':
+		received_command = request.REQUEST["command"]
+		print received_command
+		return HttpResponse("POST OK")
