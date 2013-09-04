f9ee7491f2f9a55647b4b334648b4e08a6f9ed64
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-04 14:53:46 +0200

    Changes to remotelly do POST and test it

--- RELab/views.py
+++ RELab/views.py
@@ -4,19 +4,31 @@ import json
 import urllib2
 import urllib
 
-URL = "http://www.google.es"
+URL = 'http://192.168.0.140'
+RESET_state = "OFF"
 
 def home(request):
 	if request.method == 'GET':
 		return render(request, 'home.html')
 	elif request.method == 'POST':
-		received_command = request.REQUEST["command"]
-		print received_command
-		post_data = {command:received_command.encode('utf-8')}
-		print type(post_data)
-		post_data_encoded = urllib.urlencode(post_data)
-		request = urllib2.Request(URL, post_data)
-		response = urllib2.urlopen(request)
-		the_page = response.read()
-		#print response.read()
-		return HttpResponse("POST OK")
+		print request.REQUEST
+		try:
+			received_command = request.REQUEST["command"]
+			print received_command
+			if received_command == "RESET":
+				if RESET_state == "OFF":
+					received_command = "PUL7=ON"
+					RESET_state = "ON"
+				else:
+					received_command = "PUL7=OFF"
+					RESET_state = "OFF"
+			query_args = { '': received_command }
+			encoded_args = urllib.urlencode(query_args)
+			try:
+				urllib2.urlopen(URL, encoded_args).read()
+			except:
+				print "Connection Refused"
+			return HttpResponse("POST to Server OK")
+		except:
+			print "Invalid argument"
+			return HttpResponse("POST to Server OK. Invalid argument")		
